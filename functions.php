fbc20865c9fc49e3960a3efd31cbdcc2d48dac20
giogandola98 <giogandola@gmail.com>
2018-04-14 13:39:35 +0200

    First commit

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+function register($username,$hash_pass)
+{
+  //if ok
+  return true;
+  else 
+    return false;
+}
+function login($hash_user,$hash_pass)
+{
+  //if ok
+  return true;
+  else {
+    return false;
+  }
+}
+ ?>
---
+++ search.php
@@ -0,0 +1,11 @@
+<?php
+if(isset($_POST['name']))
+{
+  if(isset($_POST['lat_lng']))
+   //search for logation in range 
+  else
+  {
+
+  }
+}
+ ?>
